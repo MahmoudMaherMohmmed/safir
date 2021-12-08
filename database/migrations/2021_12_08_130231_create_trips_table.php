7b17b7e9ce28bf9483db611da4ab8b2731aab7c0
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-12-08 15:15:32 +0200

    trips model

---
+++ database/migrations/2021_12_08_130231_create_trips_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('descrription');
+            $table->string('price');
+            $table->string('persons_count');
+			$table->string('image');
+            $table->integer('category_id')->unsigned();
+            $table->integer('country_id')->unsigned();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('country_id')->references('id')->on('countries')->onUpdate('CASCADE')->onDelete('CASCADE');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
