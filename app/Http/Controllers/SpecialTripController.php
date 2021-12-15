559bc332cf72a667651df246ac814660f9caf191
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-12-15 10:11:09 +0200

    special trip

---
+++ app/Http/Controllers/SpecialTripController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpecialTrip;
+use Illuminate\Http\Request;
+
+class SpecialTripController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SpecialTrip  $specialTrip
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SpecialTrip $specialTrip)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SpecialTrip  $specialTrip
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SpecialTrip $specialTrip)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SpecialTrip  $specialTrip
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SpecialTrip $specialTrip)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SpecialTrip  $specialTrip
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SpecialTrip $specialTrip)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_12_15_080014_create_special_trips_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('special_trips', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('client_id')->unsigned();
+            $table->integer('country_id')->unsigned();
+            $table->string('start_day');
+            $table->string('days_count');
+            $table->string('persons_count');
+            $table->text('description');
+            $table->integer('status')->default(0);
+
+            $table->foreign('client_id')->references('id')->on('clients')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('country_id')->references('id')->on('countries')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('special_trips');
+    }
+}
