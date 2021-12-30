bdcfb8731b2cedd5bd2888e21e4c95be38fb4f76
Mahmoud Maher <mahmoud_maher12345@yahoo.com>
2021-12-30 10:45:03 +0200

    update my reservation

--- app/Http/Controllers/Api/TripController.php
+++ app/Http/Controllers/Api/TripController.php
@@ -41,7 +41,7 @@ class TripController extends Controller
         $client_id = $request->user()->id;
         $reservations_array = [];
 
-        $reservations = Reservation::where('client_id', $client_id)->get();
+        $reservations = Reservation::where('client_id', $client_id)->whereDate('to', '>', Carbon::now())->get();
         if(isset($reservations) && $reservations!=null){
             foreach($reservations as $reservation){
                 if(isset($reservation->trip) && $reservation->trip!=null)
@@ -58,7 +58,7 @@ class TripController extends Controller
         $client_id = $request->user()->id;
         $reservations_array = [];
 
-        $reservations = Reservation::where('client_id', $client_id)->where('status', '!=', 1)->get();
+        $reservations = Reservation::where('client_id', $client_id)->whereDate('to', '<', Carbon::now())->where('status', '!=', 1)->get();
         if(isset($reservations) && $reservations!=null){
             foreach($reservations as $reservation){
                 if(isset($reservation->trip) && $reservation->trip!=null)
